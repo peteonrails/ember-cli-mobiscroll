cf2e332f6a0a352bdd91456d7b52a17cfb431cb0
Peter Jackson <pete@peteonrails.com>
2016-02-25 17:23:30 -0500

    Mobiscroll dependencies

--- index.js
+++ index.js
@@ -2,5 +2,18 @@
 'use strict';
 
 module.exports = {
-  name: 'ember-cli-mobiscroll'
+  name: 'ember-cli-mobiscroll',
+  included: function(app) {
+    this._super.included(app);
+    app.import(app.bowerDirectory + '/mobiscroll/js/mobiscroll.appframework.js');
+    app.import(app.bowerDirectory + '/mobiscroll/js/mobiscroll.core.js');
+    app.import(app.bowerDirectory + '/mobiscroll/js/mobiscroll.datetime.js');
+    app.import(app.bowerDirectory + '/mobiscroll/js/mobiscroll.datetimebase.js');
+    app.import(app.bowerDirectory + '/mobiscroll/js/mobiscroll.scroller.js');
+    app.import(app.bowerDirectory + '/mobiscroll/css/mobiscroll.appframework.css');
+    app.import(app.bowerDirectory + '/mobiscroll/css/mobiscroll.core.css');
+    app.import(app.bowerDirectory + '/mobiscroll/css/mobiscroll.datetime.css');
+    app.import(app.bowerDirectory + '/mobiscroll/css/mobiscroll.datetimebase.css');
+    app.import(app.bowerDirectory + '/mobiscroll/css/mobiscroll.scroller.css');
+  },
 };
